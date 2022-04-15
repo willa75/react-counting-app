43276f0d3c2fecf13b63e52f89a397abad82e5ce
willa75 <anthonywi99@yahoo.com>
2022-04-15 13:29:48 -0400

    Added envrionment variable for key

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,34 @@ export type ValueObject = {
 }
 
 function App() {
-  const [count, setCount] = useState<Count>({id: '1ccb732e-b55a-4404-ad3f-0f99c02fe44e', value: '219'});
+  const [count, setCount] = useState<Count>({id: process.env.REACT_APP_COUNT_KEY, value: '0'});
 
   useEffect(() => {
-    const fetchCount =async () => {
-      const newCount = await getData<ValueObject>('https://api.countapi.xyz/get/1ccb732e-b55a-4404-ad3f-0f99c02fe44e');
+    const fetchCount = async () => {
+      const count = await getData<ValueObject>(`https://api.countapi.xyz/get/${process.env.REACT_APP_COUNT_KEY}`);
       setCount({
-        id: '1ccb732e-b55a-4404-ad3f-0f99c02fe44e',
-        value: newCount.value
+        id: process.env.REACT_APP_COUNT_KEY,
+        value: count.value
       });
     }
 
     fetchCount();
   }, []);
 
-
+  const increaseCount = async (): Promise<void> => {
+    const newCount = await getData<ValueObject>(`https://api.countapi.xyz/hit/${process.env.REACT_APP_COUNT_KEY}`);
+    setCount({
+      id: process.env.REACT_APP_COUNT_KEY,
+      value: newCount.value
+    });
+  };
 
   return (
     <div className="App">
       <h1 className='app-title'>Counting App</h1>
       <div>
         <h2>{ count.value }</h2>
+        <button onClick={increaseCount}>Increase Value</button>
       </div>
     </div>
   );
