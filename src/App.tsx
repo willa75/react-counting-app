5f1b0882a8b4bef049c02ba80ea2fa7c61aad429
willa75 <anthonywi99@yahoo.com>
2022-04-15 12:59:10 -0400

    Added count with value from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getData } from './utils/data.utils';
 import './App.css';
 
+export type Count = {
+  id: string;
+  value: string;
+}
+
+export type ValueObject = {
+  value: string;
+}
+
 function App() {
+  const [count, setCount] = useState<Count>({id: '1ccb732e-b55a-4404-ad3f-0f99c02fe44e', value: '219'});
+
+  useEffect(() => {
+    const fetchCount =async () => {
+      const newCount = await getData<ValueObject>('https://api.countapi.xyz/get/1ccb732e-b55a-4404-ad3f-0f99c02fe44e');
+      setCount({
+        id: '1ccb732e-b55a-4404-ad3f-0f99c02fe44e',
+        value: newCount.value
+      });
+    }
+
+    fetchCount();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app-title'>Counting App</h1>
+      <div>
+        <h2>{ count.value }</h2>
+      </div>
     </div>
   );
 }
